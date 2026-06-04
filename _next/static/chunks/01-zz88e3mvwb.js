f5ca3782c33d625644a76f3921da74dce9ecda32
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2026-06-04 13:54:45 +0200

    Updates

--- _next/static/chunks/01-zz88e3mvwb.js
+++ _next/static/chunks/01-zz88e3mvwb.js
@@ -1,11 +1,11 @@
 __turbopack_load_page_chunks__("/_app", [
   "static/chunks/34gp3plpyeaai.js",
-  "static/chunks/39xskpyb82u17.js",
+  "static/chunks/2ew7i1vn3fceu.js",
   "static/chunks/2vvbe9ifa_wjz.js",
   "static/chunks/2nqe4r_4zqdp0.js",
   "static/chunks/3v7iw-6j7avks.js",
   "static/chunks/2gue737jkznr-.css",
   "static/chunks/3yeurz0vauejv.css",
   "static/chunks/0spkdxstmrxb2.css",
-  "static/chunks/turbopack-20a_4_e4n89ce.js"
+  "static/chunks/turbopack-2ofmf-2b1jsav.js"
 ])
